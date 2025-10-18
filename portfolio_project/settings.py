2335a5972b5551d62489fbc3bbeabc2bab3de29b
johnaish <jmaish819@gmail.com>
2025-10-18 09:32:34 +0300

    Added Render domain to ALLOWED_HOSTS

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -7,7 +7,8 @@ SECRET_KEY = 'replace-this-with-a-secure-secret-for-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://johnmainaportfolio.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 INSTALLED_APPS = [
